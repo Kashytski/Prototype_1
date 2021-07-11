aca5cb21d607c350d774b98d16a9310039661361
Ivan <ivan.kashytski.mkx@gmail.com>
2021-07-11 22:52:09 +0300

    load_on_git

---
+++ Assets/Course Library/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 20f;
+    private float turnSpeed = 40f;
+    private float horizontalInput;
+    private float verticalInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        
+        //Move vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        //Turn vehicle
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * verticalInput * horizontalInput);
+    }
+}
